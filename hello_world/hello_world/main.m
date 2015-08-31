be5f85cbd9b80d40e7fcfdd17f32621d69f7fae2
Hank Su <susiningalive@outlook.com>
2015-08-31 12:01:15 +0800

    Add hello world proj

---
+++ hello_world/hello_world/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  hello_world
+//
+//  Created by 苏思宁 on 15/8/31.
+//  Copyright (c) 2015年 SSN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
